f0f0a7f188d0283c7bfce2a33c29f9ca840b3226
Kirill Grachev <pllayer@mail.ru>
2024-10-25 18:43:31 +0700

    upd install deps scripts. support install depends on ubuntu 20

--- install_depends_ubuntu22.sh
+++ install_depends_ubuntu22.sh
@@ -1,11 +1,9 @@
-# часть пакетов уже удалена из основного репозитория Ubuntu 22. Нужно подключить подключить репозиторий от Ubuntu 20.
-# /etc/apt/sources.list нужно добавить строки
-# deb http://mirror.docker.ru/ubuntu/ focal main
-# deb http://mirror.docker.ru/ubuntu/ focal-updates main
-# и сделать apt update
+cat /etc/*release | grep 'DISTRIB_RELEASE=22.04' &> /dev/null
 
-sudo add-apt-repository 'deb [arch=amd64] http://ru.archive.ubuntu.com/ubuntu/ focal main' 
-apt update
+if [ $? == 0 ]; then
+   sudo add-apt-repository 'http://ru.archive.ubuntu.com/ubuntu focal/main amd64'
+   apt update
+fi
 
 sudo apt install -y autoconf automake xutils-dev libtool libpixman-1-dev x11proto-bigreqs-dev x11proto-composite-dev \
 		    x11proto-dri3-dev x11proto-present-dev x11proto-resource-dev x11proto-scrnsaver-dev x11proto-fonts-dev \
