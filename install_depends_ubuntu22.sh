994c6c5f4900f2bb5bc8361d74c57cc8013674d6
Kirill Grachev <pllayer@mail.ru>
2024-10-25 16:45:52 +0700

    Upd

--- install_depends_ubuntu22.sh
+++ install_depends_ubuntu22.sh
@@ -4,9 +4,16 @@
 # deb http://mirror.docker.ru/ubuntu/ focal-updates main
 # и сделать apt update
 
-sudo apt install autoconf automake xutils-dev libtool libpixman-1-dev x11proto-bigreqs-dev x11proto-composite-dev \
-		 x11proto-dri3-dev x11proto-present-dev x11proto-resource-dev x11proto-scrnsaver-dev x11proto-fonts-dev \
-		 x11proto-xf86dri-dev x11proto-xcmisc-dev x11proto-record-dev xfonts-utils x11-xkb-utils libxfont-dev \
-		 freeglut3-dev mesa-common-dev
+sudo add-apt-repository 'deb [arch=amd64] http://ru.archive.ubuntu.com/ubuntu/ focal main' 
 
+sudo apt install -y autoconf automake xutils-dev libtool libpixman-1-dev x11proto-bigreqs-dev x11proto-composite-dev \
+		    x11proto-dri3-dev x11proto-present-dev x11proto-resource-dev x11proto-scrnsaver-dev x11proto-fonts-dev \
+		    x11proto-xf86dri-dev x11proto-xcmisc-dev x11proto-record-dev xfonts-utils x11-xkb-utils libxfont-dev \
+		    freeglut3-dev mesa-common-dev
+
+git clone git://anongit.freedesktop.org/xorg/lib/libxtrans -b xtrans-1.3.5
+cd libxtrans && mkdir -p output && cd output
+../autogen.sh --prefix=/opt/ogon
+make && sudo make install
+cd ../..
          
