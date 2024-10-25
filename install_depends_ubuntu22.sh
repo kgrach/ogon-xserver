65f281e8e2eeb6abb077e5da47d41c0d6b13c199
Kirill Grachev <pllayer@mail.ru>
2024-10-25 16:57:45 +0700

    Upd again

--- install_depends_ubuntu22.sh
+++ install_depends_ubuntu22.sh
@@ -5,15 +5,17 @@
 # и сделать apt update
 
 sudo add-apt-repository 'deb [arch=amd64] http://ru.archive.ubuntu.com/ubuntu/ focal main' 
+apt update
 
 sudo apt install -y autoconf automake xutils-dev libtool libpixman-1-dev x11proto-bigreqs-dev x11proto-composite-dev \
 		    x11proto-dri3-dev x11proto-present-dev x11proto-resource-dev x11proto-scrnsaver-dev x11proto-fonts-dev \
 		    x11proto-xf86dri-dev x11proto-xcmisc-dev x11proto-record-dev xfonts-utils x11-xkb-utils libxfont-dev \
 		    freeglut3-dev mesa-common-dev
 
-git clone git://anongit.freedesktop.org/xorg/lib/libxtrans -b xtrans-1.3.5
-cd libxtrans && mkdir -p output && cd output
+sudo apt install libfontenc-dev
+
+git clone https://gitlab.freedesktop.org/xorg/lib/libxfont.git --branch libXfont-1.5-branch
+cd libxfont/ && mkdir -p output && cd output
 ../autogen.sh --prefix=/opt/ogon
 make && sudo make install
-cd ../..
-         
+cd ../..         
