d9e1ecda8f5ee493fc0fc8b4e1b46ac27394fbe7
Krill Grachev <pllayer@mail.ru>
2024-10-25 20:01:23 +0700

    Upd

--- install_depends_ubuntu22.sh
+++ install_depends_ubuntu22.sh
@@ -1,8 +1,8 @@
 cat /etc/*release | grep 'DISTRIB_RELEASE=22.04' &> /dev/null
 
 if [ $? == 0 ]; then
-   sudo add-apt-repository 'http://ru.archive.ubuntu.com/ubuntu focal/main amd64'
-   apt update
+   sudo add-apt-repository 'deb [arch=amd64] http://ru.archive.ubuntu.com/ubuntu focal main'
+   sudo apt update
 fi
 
 sudo apt install -y autoconf automake xutils-dev libtool libpixman-1-dev x11proto-bigreqs-dev x11proto-composite-dev \
@@ -10,7 +10,7 @@ sudo apt install -y autoconf automake xutils-dev libtool libpixman-1-dev x11prot
 		    x11proto-xf86dri-dev x11proto-xcmisc-dev x11proto-record-dev xfonts-utils x11-xkb-utils libxfont-dev \
 		    freeglut3-dev mesa-common-dev
 
-sudo apt install libfontenc-dev
+sudo apt install -y libfontenc-dev
 
 git clone https://gitlab.freedesktop.org/xorg/lib/libxfont.git --branch libXfont-1.5-branch
 cd libxfont/ && mkdir -p output && cd output
